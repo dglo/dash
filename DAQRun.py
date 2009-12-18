3ecc21f5c337e1a2bceafd0e9b73c999414f4b08
Dave Glowacki <dglo@icecube.wisc.edu>
2009-12-18 05:22:40 -0600

    Issue 3670: Reset Active DOM calculation after each run

--- DAQRun.py
+++ DAQRun.py
@@ -51,7 +51,7 @@ else:
 sys.path.append(join(metaDir, 'src', 'main', 'python'))
 from SVNVersionInfo import get_version_info
 
-SVN_ID  = "$Id: DAQRun.py 4817 2009-12-18 17:18:55Z dglo $"
+SVN_ID  = "$Id: DAQRun.py 4818 2009-12-18 17:22:40Z dglo $"
 
 # Find install location via $PDAQ_HOME, otherwise use locate_pdaq.py
 if os.environ.has_key("PDAQ_HOME"):
@@ -617,6 +617,7 @@ class DAQRun(object):
             self.__activeDOMDetail = self.setup_timer(DAQRun.ACTIVERPT_NAME,
                                                       DAQRun.ACTIVERPT_PERIOD)
         self.__activeDOMThread   = None
+        self.__badActiveDOMCount   = None
 
         self.__liveInfo       = None
         self.__id = int(time.time())
@@ -1148,17 +1149,17 @@ class DAQRun(object):
             if self.__activeMonitor is not None and \
                     self.__activeDOMThread is not None and \
                     not self.__activeDOMThread.done():
-                self.badActiveDOMCount += 1
-                if self.badActiveDOMCount <= 3:
+                self.__badActiveDOMCount += 1
+                if self.__badActiveDOMCount <= 3:
                     self.log.error(("WARNING: Active DOM thread" +
                                     " is hanging (#%d)") %
-                                   self.badActiveDOMCount)
+                                   self.__badActiveDOMCount)
                 else:
                     self.log.error("ERROR: Active DOM monitoring seems to be" +
                                    " stuck, monitoring will not be done")
                     self.__activeDOMTimer = None
             else:
-                self.badActiveDOMCount = 0
+                self.__badActiveDOMCount = 0
 
                 sendDetails = False
                 if self.__activeDOMDetail is not None and \
