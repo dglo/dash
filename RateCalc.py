036548c9f974d87e0964f510931b55f3e6d39ead
Dave Glowacki <dglo@icecube.wisc.edu>
2011-07-13 16:09:31 -0500

    Issue 5022: Lose trailing whitespace

--- RateCalc.py
+++ RateCalc.py
@@ -13,7 +13,7 @@ class RateCalcEntry(object):
     def __repr__(self):
         return "RateCalcEntry[%s -> %s]" % (str(self.time), str(self.n))
     def __str__(self): return "%s: %s" % (self.time, self.n)
-    
+
 def dt(t0, t1): # Calculate absolute time delta in seconds from datetime objects
     d = t1-t0
     return d.days*86400 + d.seconds + 1.E-6*d.microseconds
@@ -33,7 +33,7 @@ class RateCalc(object):
             (self.interval, self.maxentries, str(self.entries))
 
     def reset(self): self.entries = []
-    
+
     def add(self, time, count):
         """
         Add new entry to list, but don't let list exceed self.maxentries
@@ -41,7 +41,7 @@ class RateCalc(object):
         while len(self.entries) >= self.maxentries:
             self.entries.pop(0)
         self.entries.append(RateCalcEntry(time, count))
-        
+
     def rate(self):
         """
         Get latest rate value.  Raise exceptions if time difference
@@ -61,7 +61,7 @@ class RateCalc(object):
                 if dtsec > self.interval: break
         if latest is None or dtsec == 0.0: return 0.0
         return float(latest.n - entry.n)/float(dtsec)
-    
+
 def main():
     """
     Test by running at 1 Hz for 10 seconds, then hammering it for one second, then
@@ -88,6 +88,6 @@ def main():
         count += 1
         rc.add(datetime.datetime.now(), count)
         print rc.rate()
-    
+
 if __name__ == "__main__": main()
 
