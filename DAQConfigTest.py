7e3b5e6c1729f4c55f5e77211a77e3317dd1de77
Dave Glowacki <dglo@icecube.wisc.edu>
2013-03-25 11:19:29 -0500

    Issue 6178: Add unit test for DAQConfig.basename()

--- DAQConfigTest.py
+++ DAQConfigTest.py
@@ -41,6 +41,18 @@ class DAQConfigTest(unittest.TestCase):
                              'For string %d pos %d, expected %s, not %s' %
                              (data[2], data[3], data[0], dom))
 
+    def testNames(self):
+        cfgDir = os.path.join(self.initPDAQHome(), "config")
+
+        for n in ("simpleConfig", "sps-IC40-IT6-AM-Revert-IceTop-V029"):
+            cfg = DAQConfigParser.load(n, cfgDir)
+            self.assertEqual(n, cfg.basename(),
+                             "Expected %s, not %s" % (n, cfg.basename()))
+            fullname = os.path.join(cfgDir, n + ".xml")
+            self.assertEqual(fullname, cfg.configFile(),
+                             "Expected %s, not %s" %
+                             (fullname, cfg.configFile()))
+
     def testListsSim5(self):
         metaDir = self.initPDAQHome()
         cfg = DAQConfigParser.load("simpleConfig", metaDir + "/config")
@@ -65,7 +77,7 @@ class DAQConfigTest(unittest.TestCase):
     def testLookupSim5(self):
         metaDir = self.initPDAQHome()
         cfg = DAQConfigParser.load("simpleConfig", metaDir + "/config")
-        
+
         dataList = (('53494d550101', 'Nicholson_Baker', 1001, 1),
                     ('53494d550120', 'SIM0020', 1001, 20),
                     ('53494d550140', 'SIM0040', 1001, 40),
