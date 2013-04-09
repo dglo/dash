88f9a21a5e53d33f2ae46f979d40f577faea2089
Matt Newcomb <mnewcomb@icecube.wisc.edu>
2013-04-09 09:03:38 -0500

    Issue 6143: Make run config code be backwards compatible to the old syntax

--- RemoveHubs.py
+++ RemoveHubs.py
@@ -7,6 +7,7 @@ import sys
 from utils import ip
 
 from DAQConfig import DAQConfig, DAQConfigParser
+from DAQConfig import DAQConfigException
 from locate_pdaq import find_pdaq_config
 
 def getHubName(num):
