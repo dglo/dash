5a7d9e8c035c8e330ff65f4e561932485b3fe030
Dave Glowacki <dglo@icecube.wisc.edu>
2014-12-22 08:50:19 -0600

    Handle all of Java program's options

--- DumpPayloads.py
+++ DumpPayloads.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
+#
+# Dump all payloads in the data file
 
 from RunJava import runJava
 
 def add_arguments(parser):
+    parser.add_argument("-D", "--configDir", dest="configDir",
+                        help="Configuration directory")
+    parser.add_argument("-f", "--fullDump", dest="fullDump",
+                        action="store_true", default=False,
+                        help="Dump entire payload")
+    parser.add_argument("-H", "--hexDump", dest="hexDump",
+                        action="store_true", default=False,
+                        help="Dump payload bytes as hexadecimal blocks")
+    parser.add_argument("-n", "--numToDump", type=int, dest="numToDump",
+                        default=None,
+                        help="Maximum number of payloads to dump")
+    parser.add_argument("-r", "--configName", dest="configName",
+                        help="Run configuration name")
     parser.add_argument(dest="fileList", nargs="+")
 
 def dump_payloads(args):
@@ -11,7 +26,22 @@ def dump_payloads(args):
     mavenDeps = [("log4j", "log4j", "1.2.7"),
                  ("commons-logging", "commons-logging", "1.0.3")]
 
-    runJava(app, ["-mx2000m"], args.fileList, daqProjects, mavenDeps)
+    arglist = []
+    if args.configDir is not None:
+        arglist += ["-D", args.configDir]
+    if args.fullDump:
+        arglist.append("-f")
+    if args.hexDump:
+        # note that the Java argument is lowercase 'h', not 'H'
+        arglist.append("-h")
+    if args.numToDump is not None:
+        arglist += ["-n", str(args.numToDump)]
+    if args.configName is not None:
+        arglist += ["-r", args.configName]
+
+    arglist += args.fileList
+
+    runJava(app, ["-mx2000m"], arglist, daqProjects, mavenDeps)
 
 if __name__ == "__main__":
     import argparse
