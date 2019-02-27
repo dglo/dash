d9dda9a3792a2c04292907180de1154c0e5c7050
Dave Glowacki <dglo@icecube.wisc.edu>
2019-02-27 07:57:42 -0600

    Issue 8607: Clean up complaints from pylint

--- scmversion.py
+++ scmversion.py
@@ -52,26 +52,26 @@ def __exec_cmd(cmd, shell=False, cwd=None):
     """
 
     try:
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE, shell=shell, cwd=cwd)
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE, shell=shell, cwd=cwd)
     except OSError as exc:
         raise SCMVersionError("Command: '%s' raised OSError: '%s'" %
                               (cmd, exc))
 
-    ret_code = p.wait()
+    ret_code = proc.wait()
     if ret_code != 0:
         raise SCMVersionError("Command: '%s' returned non-zero code: '%s'" %
                               (cmd, ret_code))
 
-    stdout, stderr = p.communicate()
-    if len(stderr) != 0:
+    stdout, stderr = proc.communicate()
+    if stderr is not None and stderr != "":
         raise SCMVersionError("Command: '%s' returned non-empty stderr: '%s'" %
                               (cmd, stderr))
 
     return stdout
 
 
-def __get_git_info(dir):
+def __get_git_info(svn_dir):
     """
     Gather the Git version info for the specified directory
     """
@@ -83,7 +83,7 @@ def __get_git_info(dir):
     time = None
 
     proc = subprocess.Popen(["git", "branch"], stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT, cwd=dir)
+                            stderr=subprocess.STDOUT, cwd=svn_dir)
     for line in proc.stdout:
         line = line.strip()
         if line.startswith("*"):
@@ -96,7 +96,7 @@ def __get_git_info(dir):
 
     proc = subprocess.Popen(["git", "show", "--summary"],
                             stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT, cwd=dir)
+                            stderr=subprocess.STDOUT, cwd=svn_dir)
     for line in proc.stdout:
         if line.startswith("Date:"):
             dstr = line.split(':', 1)[1].strip()
@@ -117,7 +117,7 @@ def __get_git_info(dir):
     }
 
 
-def __get_hg_info(dir):
+def __get_hg_info(svn_dir):
     """
     Gather the Mercurial version info for the specified directory
     """
@@ -129,7 +129,7 @@ def __get_hg_info(dir):
     time = None
 
     proc = subprocess.Popen(["hg", "sum"], stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT, cwd=dir)
+                            stderr=subprocess.STDOUT, cwd=svn_dir)
 
     for line in proc.stdout:
         if line.startswith("branch:"):
@@ -149,7 +149,7 @@ def __get_hg_info(dir):
 
     proc = subprocess.Popen(["hg", "log", "-r" + repo_rev],
                             stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT, cwd=dir)
+                            stderr=subprocess.STDOUT, cwd=svn_dir)
     for line in proc.stdout:
         if line.startswith("date:"):
             dstr = line.split(':', 1)[1].strip()
@@ -189,77 +189,93 @@ def __get_scmrel_from_homedir():
     return __UNRELEASED
 
 
-def __get_svn_info(dir):
+def __get_svn_externals(svn_dir, dir_url):
     """
-    Gather the Subversion version info for the specified directory,
-    including external project versions
+    Return a list of externals.
+    Each entry is a tuple containing (subdirectory, svn_url)
     """
-
-    rel = __UNRELEASED
-    repo_rev = None
-    modified = False
-    date = None
-    time = None
-
-    # First, run svnversion on the dir
-    proc = subprocess.Popen("svnversion", stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT, cwd=dir)
-    svn_rev = proc.communicate()[0].rstrip()
-    proc.stdout.close()
-
-    # Get the repo URL used by the dir (used to see if any of the
-    # externals have been switched)
-    dir_url = None
-    proc = subprocess.Popen(["svn", "info"], stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT, cwd=dir)
-    for line in proc.stdout:
-        if line.startswith(b"URL:"):
-            dir_url = line.split()[1].strip()
-            continue
-        if line.startswith(b"Last Changed Date:"):
-            dstr = line.split(b':', 1)[1].strip()
-
-            # only care about first 3 fields (date time timezone)
-            dflds = dstr.split(None, 3)[:3]
-
-            (date, time) = __parse_date_time(" ".join(str(dflds)),
-                                             "%Y-%m-%d %H:%M:%S")
-            continue
-    proc.stdout.close()
-
-    repo_url = None
-
-    if dir_url is None:
-        external_output = ""
-    else:
-        # try to pull the release name out of the URL
-        uflds = dir_url.split('/')
-        if len(uflds) > 1 and uflds[-2] == "releases":
-            rel = uflds[-1]
-
-        repo_url = '/'.join(dir_url.split('/', 3)[:3])  # up to the 3rd '/'
-
-        # Now run svnversion on each of the externals (note that svn chokes
-        # on symlinks, so using cwd)
-        external_output = __exec_cmd(["svn", "pg", "svn:externals",
-                                      "--strict"], cwd=dir)
-
-    # A list of 2-element lists: [external, tail_url]
     externals = []
+    if dir_url is None:
+        return externals
+
+    repo_url = '/'.join(dir_url.split('/', 3)[:3])  # up to the 3rd '/'
+
+    # Now run svnversion on each of the externals (note that svn chokes
+    # on symlinks, so using cwd)
+    external_output = __exec_cmd(["svn", "pg", "svn:externals",
+                                  "--strict"], cwd=svn_dir)
+
+    # build list of 2-element lists: [external, tail_url]
     for line in external_output.splitlines():
-        if len(line) == 0:
+        if line == "":
             continue
         parts = line.split()
 
         if parts[0] in EXTERNALS_TO_IGNORE:
             continue
 
-        externals.append([parts[0], parts[-1].split(repo_url)[1]])
+        externals.append((parts[0], parts[-1].split(repo_url)[1]))
 
-    # A list of running svnversion on each external
+    return externals
+
+def __get_svn_info(svn_dir):
+    """
+    Gather the Subversion version info for the specified directory,
+    including external project versions
+    """
+
+    date = None
+    time = None
+
+    # First, run svnversion on the dir
+    proc = subprocess.Popen("svnversion", stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT, cwd=svn_dir)
+    svn_rev = proc.communicate()[0].rstrip()
+    proc.stdout.close()
+
+    # Get the repo URL used by the directory (used to see if any of the
+    # externals have been switched)
+    dir_url = None
+    proc = subprocess.Popen(["svn", "info"], stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT, cwd=svn_dir)
+    for line in proc.stdout:
+        if line.startswith(b"URL:"):
+            dir_url = line.split()[1].strip()
+        elif line.startswith(b"Last Changed Date:"):
+            dstr = line.split(b':', 1)[1].strip()
+
+            # only care about first 3 fields (date time timezone)
+            bflds = dstr.split(None, 3)[:3]
+            dflds = [str(fld) for fld in bflds]
+
+            (date, time) = __parse_date_time(" ".join(dflds),
+                                             "%Y-%m-%d %H:%M:%S")
+    proc.stdout.close()
+
+    repo_rev = __get_svn_repo_revision(svn_dir, dir_url, svn_rev)
+
+    # try to pull the release name out of the URL
+    uflds = dir_url.split('/')
+    if len(uflds) > 1 and uflds[-2] == "releases":
+        rel = uflds[-1]
+    else:
+        rel = __UNRELEASED
+
+    return {
+        "release": rel,
+        "repo_rev": repo_rev,
+        "date": date,
+        "time": time,
+    }
+
+
+def __get_svn_repo_revision(svn_dir, dir_url, svn_rev):
+    # get the SVN versions from each external project
     versions = [__exec_cmd(["svnversion", "-n",
-                            os.path.join(dir, extern[0]), extern[1]])
-                for extern in externals]
+                            os.path.join(svn_dir, extern[0]), extern[1]])
+                for extern in __get_svn_externals(svn_dir, dir_url)]
+
+    # append the top-level version info
     versions.append(svn_rev)
 
     switched = modified = exported = False
@@ -289,16 +305,9 @@ def __get_svn_info(dir):
     mods = (modified and "M" or "") + (switched and "S" or "") + \
            (exported and "E" or "")
 
-    spread = high_rev > low_rev
-    repo_rev = "%d%s%s" % (low_rev, spread and (":" + str(high_rev)) or "",
-                           mods != "" and (":" + mods) or "")
-
-    return {
-        "release": rel,
-        "repo_rev": repo_rev,
-        "date": date,
-        "time": time,
-    }
+    return "%d%s%s" % (low_rev,
+                       high_rev <= low_rev and "" or (":" + str(high_rev)),
+                       mods == "" and "" or (":" + mods))
 
 
 def __make_empty_info():
@@ -306,14 +315,14 @@ def __make_empty_info():
     Create a content-free version info dictionary
     """
     empty = {}
-    for f in FIELD_NAMES:
-        if f == "release":
+    for fld in FIELD_NAMES:
+        if fld == "release":
             val = __UNRELEASED
-        elif f == "repo_rev":
+        elif fld == "repo_rev":
             val = "0:0"
         else:
             val = None
-        empty[f] = val
+        empty[fld] = val
     return empty
 
 
@@ -323,22 +332,22 @@ def __parse_date_time(datestr, fmtstr):
     Return a tuple containing a `Y-M-D` string and an `H:M:S` string
     with timezone
     """
-    dt = None
+    dttm = None
     try:
-        dt = datetime.strptime(datestr, fmtstr + " %z")
+        dttm = datetime.strptime(datestr, fmtstr + " %z")
     except ValueError:
         dflds = datestr.split()
         if len(dflds[-1]) > 0 and \
            (dflds[-1][0] == '-' or dflds[-1][0] == '+'):
             # try trimming timezone
             try:
-                dt = datetime.strptime(" ".join(dflds[:-1]), fmtstr)
+                dttm = datetime.strptime(" ".join(dflds[:-1]), fmtstr)
             except ValueError:
                 pass
-    if dt is None:
+    if dttm is None:
         raise SCMVersionError("Bad date \"%s\"" % datestr)
 
-    return (dt.strftime("%Y-%m-%d"), dt.strftime("%H:%M:%S%z"))
+    return (dttm.strftime("%Y-%m-%d"), dttm.strftime("%H:%M:%S%z"))
 
 
 def __scm_type(topdir, origdir=None):
@@ -365,22 +374,22 @@ def __scm_type(topdir, origdir=None):
     return __scm_type(parent, origdir=(origdir is None and topdir or origdir))
 
 
-def get_scmversion(dir=None):
+def get_scmversion(svn_dir=None):
     """
     Extract release, revision, date, and time information for the specified
     directory from the appropriate source control management system
     """
 
-    if dir is None:
-        dir = PDAQ_HOME
+    if svn_dir is None:
+        svn_dir = PDAQ_HOME
 
-    expanded = os.path.expanduser(dir)
-    if expanded != dir:
-        dir = expanded
+    expanded = os.path.expanduser(svn_dir)
+    if expanded != svn_dir:
+        svn_dir = expanded
 
     info = None
     try:
-        stuple = __scm_type(dir)
+        stuple = __scm_type(svn_dir)
         if stuple[0] == SCM_GIT:
             info = __get_git_info(stuple[1])
         if stuple[0] == SCM_MERCURIAL:
@@ -403,8 +412,8 @@ def get_scmversion(dir=None):
                                       (len(FIELD_NAMES), len(flds), line))
 
             saved = {}
-            for i in range(len(FIELD_NAMES)):
-                saved[FIELD_NAMES[i]] = flds[i]
+            for idx, fldname in enumerate(FIELD_NAMES):
+                saved[fldname] = flds[idx]
             info = saved
 
     if info is not None and info["release"] == __UNRELEASED:
@@ -413,7 +422,7 @@ def get_scmversion(dir=None):
     return info
 
 
-def get_scmversion_str(dir=None, info=None):
+def get_scmversion_str(svn_dir=None, info=None):
     """
     Extract release, revision, date, and time information for the specified
     directory from the appropriate source control management system and
@@ -421,7 +430,7 @@ def get_scmversion_str(dir=None, info=None):
     """
 
     if info is None:
-        info = get_scmversion(dir)
+        info = get_scmversion(svn_dir)
 
     rtnstr = None
     for fld in FIELD_NAMES:
@@ -432,22 +441,22 @@ def get_scmversion_str(dir=None, info=None):
     return rtnstr
 
 
-def store_scmversion(dir=None):
+def store_scmversion(svn_dir=None):
     """
     Calculate and store the version information in a file for later querying.
     If there is a problem getting the version info, print a warning to stderr
     and don't write anything to the file.
     """
 
-    if dir is None:
-        dir = PDAQ_HOME
+    if svn_dir is None:
+        svn_dir = PDAQ_HOME
 
-    expanded = os.path.expanduser(dir)
-    if expanded != dir:
-        dir = expanded
+    expanded = os.path.expanduser(svn_dir)
+    if expanded != svn_dir:
+        svn_dir = expanded
 
     try:
-        scmstr = get_scmversion_str(dir)
+        scmstr = get_scmversion_str(svn_dir)
     except SCMVersionError as exc:
         print("SCMVersionError: " + str(exc), file=sys.stderr)
         return ""
@@ -458,22 +467,29 @@ def store_scmversion(dir=None):
     return scmstr
 
 
-if __name__ == "__main__":
-    import optparse
+def main():
+    "Main method"
+    import argparse
+    import pprint
 
-    p = optparse.OptionParser()
-    p.add_option("-d", "--dir", type="string", dest="dir",
-                 action="store", default=PDAQ_HOME,
-                 help="Location of directory tree being checked")
-    opt, args = p.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dir", type=str, dest="path",
+                        action="store", default=PDAQ_HOME,
+                        help="Location of directory tree being checked")
+    args = parser.parse_args()
 
     rev_parent = os.path.dirname(SCM_REV_FILENAME)
     if not os.path.exists(rev_parent):
         os.makedirs(rev_parent)
 
-    print("STORED -> " + store_scmversion(opt.dir))
+    print("STORED -> %s" % store_scmversion(args.path))
 
-    info = get_scmversion(opt.dir)
-    print(str(info))
+    vinfo = get_scmversion(args.path)
+    pprt = pprint.PrettyPrinter(indent=4)
+    print("RAW -> %s" % pprt.pformat(vinfo))
 
-    print(get_scmversion_str(info=info))
+    print("FORMATTED -> %s" % get_scmversion_str(info=vinfo))
+
+
+if __name__ == "__main__":
+    main()
