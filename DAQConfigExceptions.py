786ab349e65eb00b8efb27171d6b9d5e64236185
Dave Glowacki <dglo@icecube.wisc.edu>
2019-04-19 09:09:53 -0500

    Issue 8635: Add docstrings

--- DAQConfigExceptions.py
+++ DAQConfigExceptions.py
@@ -1,22 +1,29 @@
-"""Exceptions used in DAQConfig AND in some cluster configuration parsing code
+#!/usr/bin/env python
+"""
+Exceptions used in DAQConfig AND in some cluster configuration parsing code
 """
 
 
 class DAQConfigException(Exception):
+    "Base DAQ configuration exception"
     pass
 
 
 class BadComponentName(DAQConfigException):
+    "Invalid name for component"
     pass
 
 
 class BadDOMID(DAQConfigException):
+    "Invalid ID for DOM"
     pass
 
 
 class ConfigNotSpecifiedException(DAQConfigException):
+    "No configuration was specified"
     pass
 
 
 class DOMNotInConfigException(DAQConfigException):
+    "A Dom is missing from the configuration"
     pass
