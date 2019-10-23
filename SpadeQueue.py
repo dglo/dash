96cf14f1fb593b82c3ced6da78b0aed5d35d7c90
Dave Glowacki <dglo@icecube.wisc.edu>
2019-10-23 05:02:36 -0500

    Issue 8635: Revert an over-exhuberant change

--- SpadeQueue.py
+++ SpadeQueue.py
@@ -310,7 +310,7 @@ def queue_logs(args):
     logging.basicConfig()
 
     logger = logging.getLogger("spadeQueue")
-    logger.set_level(logging.DEBUG)
+    logger.setLevel(logging.DEBUG)
 
     cluster = ClusterDescription()
     spadeDir = cluster.logDirForSpade
